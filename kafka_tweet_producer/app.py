87f502c3d0616f88d808150c8e4d9df3cd403d8a
Tobias Nagel <tobias.nagel@iubh.de>
2023-01-03 12:28:24 +0100

    Implemented complete CSV-file load

--- kafka_tweet_producer/app.py
+++ kafka_tweet_producer/app.py
@@ -1,13 +1,32 @@
-import time
 import pandas as pd
 
-def main(path):
-    df = pd.read_csv(path)
-    print(f"Shape of CSV-file: {df.shape}")
-    time.sleep(10)
+def load_dataframe(path, candidate):
+    dtype = {
+        'tweet_id': int,
+        'likes': int,
+        'retweet_count': int,
+        'user_id': int,
+        'user_followers_count': int,
+        'lat': float,
+        'long': float
+    }
+    parse_dates = ['created_at', 'collected_at']
+    df = pd.read_csv(path, dtype=dtype, parse_dates=parse_dates, lineterminator='\n')
+    df.drop_duplicates(subset=['created_at', 'tweet_id', 'user_id'], inplace=True)
+    df.sort_values('created_at', inplace=True, ignore_index=True)
+    df.insert(1, 'candidate', candidate)
+    return df
+
+def main():
+    print("Loading Biden...")
+    df_biden = load_dataframe(path='/app/data/hashtag_joebiden.csv', candidate='Biden')
+    print("Loading Trump...")
+    df_trump = load_dataframe(path='/app/data/hashtag_donaldtrump.csv', candidate='Trump')
+    print(f"Biden: {df_biden.size}")
+    print(f"Trump: {df_trump.size}")
 
 def test():
     print("Module loaded!")
 
 if __name__ == '__main__':
-    main(path='/app/data/hashtag_joebiden.csv')
+    main()
