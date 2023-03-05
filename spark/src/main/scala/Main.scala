c56e409b106b62884ec06d9377eefd39c50412e6
Tobias Nagel <tobias.nagel@iubh.de>
2023-03-05 15:24:26 +0100

    Read and count incoming tweets

--- spark/src/main/scala/Main.scala
+++ spark/src/main/scala/Main.scala
@@ -1,34 +1,45 @@
-import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions._
-import org.apache.spark.sql.streaming.Trigger
+import org.apache.spark.sql._
 
-object KafkaStreamExample {
+object Main {
+
   def main(args: Array[String]): Unit = {
+
+    // Create a SparkSession
     val spark = SparkSession.builder
-      .appName("KafkaStreamExample")
+      .appName("SparkStructuredStreamingApp")
       .master("spark://spark-master:7077")
       .getOrCreate()
 
+    // Define the log-level for debbuging purposes
+    spark.sparkContext.setLogLevel("WARN")
+
+    // Import implicits to use the $-notation
     import spark.implicits._
 
+    // Define parameters to read messages from Kafka
     val kafkaParams = Map[String, String](
       "kafka.bootstrap.servers" -> "kafka:9092",
       "subscribe" -> "trump,biden"
     )
 
+    // Read messages from Kafka
     val streamDF = spark.readStream
       .format("kafka")
       .options(kafkaParams)
       .load()
 
-    val messagesDF = streamDF.selectExpr("CAST(value AS STRING)")
+    // Count messages per topic
+    val messagesGroupedByTopic: DataFrame = streamDF
+      .groupBy($"topic")
+      .sum()
 
-    val query = messagesDF.writeStream
-      .outputMode("append")
+    // Print the result on the console
+    val consoleOutput = messagesGroupedByTopic.writeStream
+      .outputMode("update")
       .format("console")
-      .trigger(Trigger.ProcessingTime("10 seconds"))
       .start()
 
-    query.awaitTermination()
+    // Wait for the termination signal
+    consoleOutput.awaitTermination()
   }
 }
