1f3a2ca7861ce395e48c0246d217dd168e3a0144
Tobias Nagel <tobias.nagel@iubh.de>
2023-03-12 20:20:46 +0100

    Introduced ELK stack for analytics

---
+++ kafka_consumer_dev/src/app.py
@@ -0,0 +1,20 @@
+import os
+from json import loads 
+from kafka import KafkaConsumer
+
+BOOTSTRAP_SERVERS = f"{os.environ['KAFKA_ADVERTISED_HOST_NAME']}:{os.environ['KAFKA_PORT']}"
+API_VERSION = tuple(map(lambda s: int(s.strip()), os.environ['KAFKA_API_VERSION'].split(',')))
+
+consumer = KafkaConsumer(
+    'processed_tweets',
+    bootstrap_servers=BOOTSTRAP_SERVERS,
+    api_version=API_VERSION,
+    auto_offset_reset='earliest',
+    enable_auto_commit=True,
+    value_deserializer=lambda x: loads(x.decode('utf-8'))
+)
+
+if __name__ == '__main__':
+    for i, message in enumerate(consumer):
+        print(f"Message received (#{i+1}): [{message.topic}] {message.value}")
+        
