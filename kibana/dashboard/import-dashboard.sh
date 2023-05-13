a39dd5cb2188788114bf311f49b840e70088e0d4
Tobias Nagel <tobias.nagel@iubh.de>
2023-05-13 16:32:35 +0200

    Added dashboard upload and reverse proxy access

--- kibana/dashboard/import-dashboard.sh
+++ kibana/dashboard/import-dashboard.sh
@@ -15,16 +15,20 @@ echo "Kibana is ready!"
 echo "Waiting for Kibana's Import Objects API to be ready. Requests might fail initially..."
 while true; do
   set +e
-  status_code=$(curl -s -o /dev/null -w "%{http_code}" -X POST http://kibana:5601/api/saved_objects/_import -H "kbn-xsrf: true" --form file=@/assets/dashboard.ndjson)
+  response=$(curl -s -w "\n%{http_code}" -X POST http://kibana:5601/api/saved_objects/_import -H "kbn-xsrf: true" --form file=@/assets/config.ndjson)
+  status_code=$(echo "$response" | tail -n1)
+  body=$(echo "$response" | sed '$d')
   exit_code=$?
   set -e
   if [ $exit_code -eq 0 ] && [ $status_code -eq 200 ]; then
     echo "Received status code $status_code. The object import was successful!"
     break
   else
-    echo "An error occurred when importing Kibana objects. Sleep for 5 seconds and try if the API is then functional."
+    echo "An error occurred when importing Kibana objects (exit code: $exit_code, status code: $status_code). The response body is:"
+    echo "$body"
+    echo "Sleep for 5 seconds and try if the API is then functional."
     sleep 5
   fi
 done
 
-echo "The Kibana dashboard can now be used!"
+echo "The Kibana dashboard can now be used at the port of this container!"
